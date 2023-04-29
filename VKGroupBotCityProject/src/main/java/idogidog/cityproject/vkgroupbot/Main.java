1f13ad87a37387735b1064a90273f42a2a09b1b1
IDogIDog <106388567+ID0gID0g@users.noreply.github.com>
2023-04-29 20:44:40 +0300

    Add files via upload

---
+++ VKGroupBotCityProject/src/main/java/idogidog/cityproject/vkgroupbot/Main.java
@@ -0,0 +1,9 @@
+package idogidog.cityproject.vkgroupbot;
+
+import idogidog.cityproject.vkgroupbot.bot.Bot;
+
+public class Main {
+    public static void main(String[] args) {
+        new Bot("token");
+    }
+}
